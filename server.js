7b9e01f393bf28d1639ed968f6213a0dab97fb64
Txny <irakozeyves9@gmail.com>
2023-01-30 19:14:07 +0200

    chore: setup postgress and sequelize orm

---
+++ server.js
@@ -0,0 +1,14 @@
+import app from "./src/app";
+import dotenv from "dotenv";
+import { connectDB } from "./src/app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+(async () => {
+    await connectDB();
+    app.listen(process.env.PORT, () => {
+        console.log(`Server listening on port ${process.env.PORT}`);
+    });
+})();
