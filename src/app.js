86d6b9fafc9017064418c6d68c7d41d7308ce34c
Txny <irakozeyves9@gmail.com>
2023-02-02 12:15:46 +0200

    feature src: add login endpoint

--- src/app.js
+++ src/app.js
@@ -11,13 +11,13 @@ dotenv.config();
 const app = express();
 
 export const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connection established successfully');
-  } catch (err) {
-    console.log(`Database connection failed: ${err}`);
-    process.exit(1);
-  }
+	try {
+		await sequelize.authenticate();
+		console.log('Database connection established successfully');
+	} catch (err) {
+		console.log(`Database connection failed: ${err}`);
+		process.exit(1);
+	}
 };
 
 app.use(morgan('dev'));
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,21 +2,38 @@ import { UserService } from '../services/user.service.js';
 import { BcryptUtility } from '../utils/bcrypt.util.js';
 import { JwtUtility } from '../utils/jwt.util.js';
 
-export class RegisterController {
-  static async registerUser(req, res) {
-    try {
-      const user = { ...req.body };
-      user.password = await BcryptUtility.hashPassword(req.body.password);
-      const { id, email } = await UserService.register(user);
-      const userData = { id, email };
-      const userToken = JwtUtility.generateToken(userData);
-      return res.status(201).json({ user: userData, token: userToken });
-    } catch (err) {
-      return res.status(500).json({
-        error: err.message,
-        message: 'Failed to register a new user',
-      });
-    }
-  }
-}
+export class UserController {
+	static async registerUser(req, res) {
+		try {
+			const user = { ...req.body };
+			user.password = await BcryptUtility.hashPassword(req.body.password);
+			const { id, email } = await UserService.register(user);
+			const userData = { id, email };
+			const userToken = JwtUtility.generateToken(userData);
+			return res.status(201).json({ user: userData, token: userToken });
+		} catch (err) {
+			return res.status(500).json({
+				error: err.message,
+				message: 'Failed to register a new user',
+			});
+		}
+	}
 
+	static async loginUser(req, res) {
+		try {
+			const {id, email, firstname, lastname, gender, role, status, avatar, verified} = req.user;
+
+			const userData = { id, email, role, firstname, lastname, gender, status, avatar, verified };
+			const token = JwtUtility.generateToken(userData);
+			return res.status(200).json({
+				token: token,
+				message: 'Login successful',
+				user: userData,
+			});
+		} catch (err) {
+			return res
+				.status(500)
+				.json({ error: err.message, message: 'Error occurred while signing in, try again' });
+		}
+	}
+}
