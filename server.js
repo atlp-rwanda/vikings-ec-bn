c342e9ffc4adb1584961fab6af50e743d5a95f28
Fidela1 <fidelatuyizere4@gmail.com>
2023-02-02 11:03:17 +0200

    chore:configure eslint and houndci

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-import app from "./src/app";
-import dotenv from "dotenv";
-import { connectDB } from "./src/app";
+import dotenv from 'dotenv';
+import app, { connectDB } from './src/app';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 (async () => {
-    await connectDB();
-    app.listen(process.env.PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
-    });
+	await connectDB();
+	app.listen(process.env.PORT, () => {
+		console.log(`Server listening on port ${PORT}`);
+	});
 })();
