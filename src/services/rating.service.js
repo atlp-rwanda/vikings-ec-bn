af2521184d333c216d9e3a1131c327a0d8b1ab1b
Txny <irakozeyves9@gmail.com>
2023-04-20 20:10:41 +0200

    fix: ratings

--- src/services/rating.service.js
+++ src/services/rating.service.js
@@ -1,18 +1,32 @@
-
 import { Ratings } from '../database/models/index';
+import { User } from '../database/models';
 
 export class RatingService {
-   
-  static async createRatings(rate) {
-    return await Ratings.create(rate);
-  }
- static async getProductRatingByField(condition){
- return await Ratings.findOne({
-    where: {...condition},
-    });
-    
-}
-static async updateRatings(fields, id) {
-  return await Ratings.update({ ...fields }, { where: { id: id } });
-}
+    static async createRatings(rate) {
+        return await Ratings.create(rate);
+    }
+
+    static async getProductRatingByField(condition) {
+        return await Ratings.findAll({
+            where: { ...condition },
+            include: [
+                {
+                    model: User,
+                    as: 'buyer',
+                    attributes: {
+                        exclude: [
+                            'password',
+                            'authCode',
+                            'mustUpdatePassword',
+                            'lastTimePasswordUpdated',
+                        ],
+                    },
+                },
+            ],
+        });
+    }
+
+    static async updateRatings(fields, id) {
+        return await Ratings.update({ ...fields }, { where: { id: id } });
+    }
 }
